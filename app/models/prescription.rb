30b65e45fb41543b7c29337c3c9640cd7cd1729a
eaylward8 <eaylward8@gmail.com>
2016-04-11 16:27:03 -0400

    Add scheduled doses when creating new prescription

--- app/models/prescription.rb
+++ app/models/prescription.rb
@@ -16,26 +16,6 @@
 #  dose_size     :string
 #
 
-  # == Schema Information
-#
-# Table name: prescriptions
-#
-#  id            :integer          not null, primary key
-#  dosage        :string
-#  doses         :integer
-#  doses_per_day :integer
-#  refills       :integer
-#  fill_duration :integer
-#  start_date    :date
-#  end_date      :date
-#  doctor_id     :integer
-#  pharmacy_id   :integer
-#  user_id       :integer
-#  drug_id       :integer
-#  created_at    :datetime         not null
-#  updated_at    :datetime         not null
-#
-
 class Prescription < ActiveRecord::Base
   belongs_to :drug
   belongs_to :doctor
