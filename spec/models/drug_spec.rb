28ed03c51171d7025f5c23727a7698a4b6302d54
eaylward8 <eaylward8@gmail.com>
2016-04-13 17:59:00 -0400

    Adds factories for all models and validation tests

--- spec/models/drug_spec.rb
+++ spec/models/drug_spec.rb
@@ -9,3 +9,46 @@
 #  updated_at :datetime         not null
 #
 
+require 'rails_helper'
+
+describe 'Drug' do 
+
+  describe 'factory' do 
+    it 'has a valid factory' do 
+      expect(build(:drug)).to be_valid
+    end
+  end
+
+  describe 'validations' do 
+
+    it 'is invalid without a name' do 
+      drug = build(:drug, name: nil)
+      drug.valid?
+
+      expect(drug.errors[:name]).to include("can't be blank")
+    end
+
+    it 'is invalid without a RXCUI number' do 
+      drug = build(:drug, rxcui: nil)
+      drug.valid?
+
+      expect(drug.errors[:rxcui]).to include("can't be blank")
+    end
+
+    it 'is invalid if the RXCUI is not a number (Fixnum)' do 
+      drug = build(:drug)
+      
+      expect(drug.rxcui.class).to eq(Fixnum)
+    end
+
+  end
+
+end
+
+
+
+
+
+
+
+
--- spec/models/prescription_spec.rb
+++ spec/models/prescription_spec.rb
@@ -16,3 +16,57 @@
 #  updated_at    :datetime         not null
 #  dose_size     :string
 #
+
+require 'rails_helper'
+
+describe 'Prescription' do 
+
+  describe 'factory' do 
+    it 'has a valid factory' do 
+      expect(build(:prescription)).to be_valid
+    end
+  end
+
+  describe 'validations' do 
+
+    it 'is invalid without a dose size' do 
+      rx = build(:prescription, dose_size: nil)
+      rx.valid?
+
+      expect(rx.errors[:dose_size]).to include("can't be blank")
+    end
+
+    it 'is invalid without a fill duration' do 
+      rx = build(:prescription, fill_duration: nil)
+      rx.valid?
+
+      expect(rx.errors[:fill_duration]).to include("can't be blank")
+    end
+
+    it 'is invalid without a number of refills' do 
+      rx = build(:prescription, refills: nil)
+      rx.valid?
+
+      expect(rx.errors[:refills]).to include("can't be blank")
+    end
+
+    it 'is invalid without a start date' do 
+      rx = build(:prescription, start_date: nil)
+      rx.valid?
+
+      expect(rx.errors[:start_date]).to include("can't be blank")
+    end
+
+
+
+  end
+
+end
+
+
+
+
+
+
+
+
