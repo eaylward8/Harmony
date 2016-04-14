6c4b7279ea25452f8f74ecf2cdcf321c0562d9f0
eaylward8 <eaylward8@gmail.com>
2016-04-14 08:38:35 -0400

    Adds code to user factory to create user with many prescriptions, drs, drugs, pharmacies

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -5,7 +5,18 @@ FactoryGirl.define do
     last_name { Faker::Name.last_name }
     email { Faker::Internet.email }
     password 'password123'
+
+    trait :with_many_prescriptions do 
+
+      transient do 
+        number_of_prescriptions 3
+      end
+
+      after(:create) do |user, evaluator|
+        create_list(:prescription, evaluator.number_of_prescriptions, user: user)
+      end
+    end
+
+
   end
-
-
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -51,26 +51,26 @@ describe 'User' do
     end
   end
 
-  context "Instance methods" do
+  describe 'object relations' do 
+    it 'can have many prescriptions' do 
+      user = create(:user, :with_many_prescriptions)
 
-    let(:user) { User.create(first_name: "John", last_name: "Johnson", email: "john@yahoo.com", password: "password123")}
+      expect(user.prescriptions.count).to eq(3)
+    end
+  end
 
-    let(:doctor) { Doctor.create(first_name: "Julius", last_name: "Erving", location: "Philadelphia") }
 
-    let(:drug) { Drug.create(name: 'Ambien', rxcui: '131725') }
-
-    let(:pharmacy) { Pharmacy.create(name: "CVS") }
-
-    let(:prescription) { Prescription.create(refills: 2, fill_duration: 7, start_date: Date.today, end_date: Date.today + 7, doctor_id: doctor.id, pharmacy_id: pharmacy.id, user_id: user.id, drug_id: drug.id, dose_size: "100mg")}
+  describe "instance methods" do
+    # create user with 2 prescriptions, doctors, drugs, & pharmacies
+    let(:user1) { create(:user, :with_many_prescriptions, number_of_prescriptions: 2) }
 
     describe "#doctors_names" do
 
-      it "returns a list of all of a user's doctors" do
-        prescription.user = user
-        prescription.doctor = doctor
-        prescription.drug = drug
-        prescription.pharmacy = pharmacy
-        expect(user.doctors_names).to eq(["1 - Dr. Julius Erving - Philadelphia"])
+      it "returns a formatted list of all of a user's doctors" do
+        d1 = user1.doctors.first
+        d2 = user1.doctors.last
+
+        expect(user1.doctors_names).to eq(["#{d1.id} - Dr. #{d1.first_name} #{d1.last_name} - #{d1.location}", "#{d2.id} - Dr. #{d2.first_name} #{d2.last_name} - #{d2.location}"])
       end
 
     end
