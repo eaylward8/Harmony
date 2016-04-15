74794723db64de99cdb0a4bda0d1aa13adaaca30
lharary <leonharary@gmail.com>
2016-04-15 13:27:55 -0400

    add sign up and login modals

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,8 +1,9 @@
 class SessionsController < ApplicationController
   skip_before_action :authorized?
   
-  def new
-  end
+  # def new
+  #   @user = User.new
+  # end
 
   def create
     user = User.find_by_email(params[:email])
