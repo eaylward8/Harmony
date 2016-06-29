3264e47c0c614926a586db31557eb0e8b32aea80
eaylward8 <eaylward8@gmail.com>
2016-06-29 08:54:42 -0400

    Fix error when user logs in with incorrect email/pw

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,11 +4,12 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by_email(params[:email])
     if user && user.authenticate(params[:password])
+      binding.pry
       session[:user_id] = user.id
       redirect_to user_path(user)
     else
-      flash.now.alert = 'Email or password is invalid'
-      render 'new'
+      flash[:error] = 'Email or password is invalid.'
+      redirect_to root_path
     end
   end
 
